cd2e0d979b1c78dcfc1ef85ab3102a9faed46c88
Davina Medina <davinamr91@gmail.com>
2023-06-15 12:47:08 +0200

    Add setting header values

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
   fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 func main() {
