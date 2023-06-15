6853886b7115d7f468f0c33be05bee7ee01c56ff
Davina Medina <davinamr91@gmail.com>
2023-06-15 12:57:43 +0200

    Add contact page

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"net/http"
 )
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html; charset=utf-8")
   fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in contact, go to  <a href=\"https://www.linkedin.com/in/davinamedina/\">Davina's linkedin</a>.</p>")
+}
+
+
 func main() {
-  http.HandleFunc("/", handlerFunc)
+  http.HandleFunc("/", homeHandler)
+  http.HandleFunc("/contact", contactHandler)
   fmt.Println("Starting the server on :3000...")
   http.ListenAndServe(":3000", nil)
 }
