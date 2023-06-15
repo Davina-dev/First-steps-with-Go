4b5864b601825686ae92f0b2e6e59dd53228151f
Davina Medina <davinamr91@gmail.com>
2023-06-15 13:41:04 +0200

    Add custom routing

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -14,10 +14,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in contact, go to  <a href=\"https://www.linkedin.com/in/davinamedina/\">Davina's linkedin</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+  switch r.URL.Path {
+  case "/":
+    homeHandler(w, r)
+  case "/contact":
+    contactHandler(w, r)
+  default:
+    fmt.Fprint(w, "Page not found")
+  }
+}
 
 func main() {
-  http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/contact", contactHandler)
+  http.HandleFunc("/",pathHandler)
   fmt.Println("Starting the server on :3000...")
   http.ListenAndServe(":3000", nil)
 }
