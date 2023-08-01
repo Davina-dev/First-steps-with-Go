399926c62063477dacf500584da3beacf43adefa
Davina Medina <davinamr91@gmail.com>
2023-08-01 14:33:19 +0200

    Starting using chi to route our app

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html; charset=utf-8")
   fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
@@ -14,19 +17,35 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in contact, go to  <a href=\"https://www.linkedin.com/in/davinamedina/\">Davina's linkedin</a>.</p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-  switch r.URL.Path {
-  case "/":
-    homeHandler(w, r)
-  case "/contact":
-    contactHandler(w, r)
-  default:
-    http.Error(w,"Page Not Found 🧐", http.StatusNotFound)
-  }
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+  </li>
+</ul>
+`)
 }
 
 func main() {
-  http.HandleFunc("/",pathHandler)
+  r := chi.NewRouter()
+  r.Get("/", homeHandler)
+  r.Get("/contact", contactHandler)
+  r.Get("/faq", faqHandler)
+  r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+    http.Error(w, "Page not found", http.StatusNotFound)
+  })
   fmt.Println("Starting the server on :3000...")
-  http.ListenAndServe(":3000", nil)
+  http.ListenAndServe(":3000", r)
 }
