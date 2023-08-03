72134b77028783f5e80aa3d50a2a58a4c862c701
Davina Medina <davinamr91@gmail.com>
2023-08-03 16:10:25 +0200

    Creating First Template

---
+++ lenslocked/cdm/exp/main.go
@@ -0,0 +1,28 @@
+package main
+
+import(
+"html/template"
+"os"
+)
+
+type User struct{
+ Name string
+ Age int
+}
+
+func main(){
+   t, err := template.ParseFiles("hello.gohtml")
+   if err != nil{
+    panic(err)
+   }
+
+user := User{
+ Name: "Davina Medina",
+ Age: 32,
+ }
+
+ err = t.Execute(os.Stdout, user)
+  if err != nil{
+     panic(err)
+  }
+}
--- lenslocked/main.go
+++ lenslocked/main.go
@@ -38,11 +38,15 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 `)
 }
 
+
+}
+
 func main() {
   r := chi.NewRouter()
   r.Get("/", homeHandler)
   r.Get("/contact", contactHandler)
   r.Get("/faq", faqHandler)
+
   r.NotFound(func(w http.ResponseWriter, r *http.Request) {
     http.Error(w, "Page not found", http.StatusNotFound)
   })
